23590f356e3d4b778d490a3f4d52a3a61b9be888
Oylsister <57207701+oylsister@users.noreply.github.com>
2024-01-18 14:58:17 +0700

    Add project files.

---
+++ ClientPreference.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace ClientPreference
+{
+    public class ClientPreference
+    {
+        private SqliteConnection Database = null!;
+
+        string DatabaseName = null!;
+
+        public async Task RegDatabase(string modulepath, string modulename, string databasename)
+        {
+            Database = new SqliteConnection($"Data Source={Path.Join(modulepath, modulename + ".db")}");
+            Database.Open();
+
+            DatabaseName = databasename;
+
+            await Database.ExecuteAsync(@"CREATE TABLE IF NOT EXISTS @DatabaseName (`SteamID` VARCHAR(64), `Value` VARCHAR(256), PRIMARY KEY (`SteamID`));", DatabaseName);
+        }
+
+        [Obsolete]
+        public async Task RegClientData(string steamid, string value)
+        {
+            if (Database == null)
+            {
+                return;
+            }
+
+            await Database.ExecuteAsync("BEGIN IF NOT EXIST (SELECT * FROM @DBName WHERE `SteamID` = @SteamId) BEGIN INSERT INTO @DatabaseName (`SteamID`, `Value`) VALUES(@steamid, @Values) END END)",
+                new
+                {
+                    DBName = DatabaseName,
+                    SteamId = steamid,
+                    Values = value
+                });
+        }
+
+        public async Task<string> GetClientData(string steamid)
+        {
+            if (Database == null)
+            {
+                return null!;
+            }
+
+            var value = await Database.QueryFirstOrDefaultAsync<string>(@"SELECT `Value` From @DBName WHERE `SteamID` = @SteamId",
+                new
+                {
+                    DBName = DatabaseName,
+                    SteamId = steamid
+                });
+
+            return value!;
+        }
+
+        public async Task SetClientData(string steamid, string value)
+        {
+            if (Database == null)
+            {
+                return;
+            }
+
+            await Database.ExecuteAsync("INSERT INTO @DBName (`SteamID`, `Value`) VALUES (@SteamId, @Values) ON CONFLICT(`SteamID`) DO UPDATE SET `Value` = @Values",
+                new
+                {
+                    SteamId = steamid,
+                    Values = value
+                });
+        }
+    }
+}
